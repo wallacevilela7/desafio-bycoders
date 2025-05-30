6e5545b690f439566fa06c564d8501c95a503651
wallacevilela7 <wallace.vilela9@gmail.com>
2025-05-30 16:36:45 -0300

    feat: Create Spring app

---
+++ src/main/java/tech/wvs/desafiobycoders/DesafioBycodersApplication.java
@@ -0,0 +1,13 @@
+package tech.wvs.desafiobycoders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioBycodersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioBycodersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/wvs/desafiobycoders/DesafioBycodersApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.wvs.desafiobycoders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioBycodersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
